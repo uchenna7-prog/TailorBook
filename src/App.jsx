7cc636ecc920625ea4b08056e7fcfc1e23569903
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-31 07:25:37 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,29 @@ import Home from './pages/Home/Home'
 import Customers from './pages/Customers/Customers'
 import CustomerDetail from './pages/CustomerDetail/CustomerDetail'
 import Tasks from './pages/Tasks/Tasks'
-import Orders from './pages/Orders/Orders' // ✅ ADD THIS
+import Orders from './pages/Orders/Orders'
 import Gallery from './pages/Gallery/Gallery'
 import Settings from './pages/Settings/Settings'
+import Profile from './pages/Profile/Profile'
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  const menuClick = () => setSidebarOpen(true)
 
   return (
     <>
       <SideBar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
       <Routes>
-        <Route path="/" element={
-          <Home onMenuClick={() => setSidebarOpen(true)} />
-        } />
-
-        <Route path="/customers" element={
-          <Customers onMenuClick={() => setSidebarOpen(true)} />
-        } />
-
-        <Route path="/tasks" element={
-          <Tasks onMenuClick={() => setSidebarOpen(true)} />
-        } />
-        <Route path="/gallery"       element={<Gallery onMenuClick={() => setSidebarOpen(true)} />} />
-
-        {/* ✅ NEW ORDERS ROUTE */}
-        <Route path="/orders" element={
-          <Orders onMenuClick={() => setSidebarOpen(true)} />
-        } />
-        <Route path="/settings"      element={<Settings     onMenuClick={() => setSidebarOpen(true)} />} />
-
-        <Route path="/customers/:id" element={
-          <CustomerDetail onMenuClick={() => setSidebarOpen(true)} />
-        } />
+        <Route path="/"              element={<Home           onMenuClick={menuClick} />} />
+        <Route path="/customers"     element={<Customers      onMenuClick={menuClick} />} />
+        <Route path="/customers/:id" element={<CustomerDetail onMenuClick={menuClick} />} />
+        <Route path="/tasks"         element={<Tasks          onMenuClick={menuClick} />} />
+        <Route path="/orders"        element={<Orders         onMenuClick={menuClick} />} />
+        <Route path="/gallery"       element={<Gallery        onMenuClick={menuClick} />} />
+        <Route path="/settings"      element={<Settings       onMenuClick={menuClick} />} />
+        <Route path="/profile"       element={<Profile        onMenuClick={menuClick} />} />
       </Routes>
     </>
   )
