0d4e3160c8e994a05db3bbf56f7f2900e5793d12
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-31 16:24:54 +0100

    Create services/authService.js

---
+++ src/services/authService.js
@@ -0,0 +1,10 @@
+import { auth } from "../firebase/firebase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+export const signup = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const login = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
