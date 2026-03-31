3430a0d2c8f1e1f889adf9875374f35051fbef78
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-31 17:55:18 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { CustomerProvider } from './contexts/CustomerContext';
-import { OrdersProvider } from './contexts/OrdersContext'; // ✅ import OrdersProvider
-import App from './App.jsx';
-import './index.css';
-import { SettingsProvider } from './contexts/SettingsContext'
-import { InvoiceProvider } from './contexts/InvoiceContext'
+// Add BrandProvider INSIDE SettingsProvider (it reads from SettingsContext)
+// Your main.jsx should look like this:
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { CustomerProvider } from './contexts/CustomerContext'
+import { SettingsProvider } from './contexts/SettingsContext'
+import { BrandProvider } from './contexts/BrandContext'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
-         <SettingsProvider>
-           <InvoiceProvider>
-      <CustomerProvider>
-        <OrdersProvider> 
-          <App />
-        </OrdersProvider>
-      </CustomerProvider>
-           </InvoiceProvider>
-         </SettingsProvider>
+      <SettingsProvider>
+        <BrandProvider>
+          <CustomerProvider>
+            <App />
+          </CustomerProvider>
+        </BrandProvider>
+      </SettingsProvider>
     </BrowserRouter>
-  </StrictMode>
-);
+  </React.StrictMode>
+)
