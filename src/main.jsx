cf7524c332f71c8c1a52a0614826773ea34643bf
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-04-01 07:57:12 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,25 @@
-
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { CustomerProvider } from './contexts/CustomerContext'
+import { AuthProvider }     from './contexts/AuthContext'
 import { SettingsProvider } from './contexts/SettingsContext'
-import { BrandProvider } from './contexts/BrandContext'
+import { BrandProvider }    from './contexts/BrandContext'
+import { CustomerProvider } from './contexts/CustomerContext'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <SettingsProvider>
-        <BrandProvider>
-          <CustomerProvider>
-            <App />
-          </CustomerProvider>
-        </BrandProvider>
-      </SettingsProvider>
+      <AuthProvider>
+        <SettingsProvider>
+          <BrandProvider>
+            <CustomerProvider>
+              <App />
+            </CustomerProvider>
+          </BrandProvider>
+        </SettingsProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
