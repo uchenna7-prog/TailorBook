07791b04db410ffcbe41007d52abb45114a3bb56
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-04-06 09:07:50 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'tailorflow-v1'
-const ASSETS = ['/','index.html']
+const ASSETS = ['/', '/index.html']
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
